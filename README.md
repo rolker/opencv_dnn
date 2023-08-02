1dea311a8a3133b9550a7e5c13dc9f9d69ac240d
Roland Arsenault <roland@ccom.unh.edu>
2023-08-02 14:45:53 -0400

    Update to work with yolov5

--- README.md
+++ README.md
@@ -2,22 +2,58 @@
 
 ## ROS noetic
 
-For building on Ubuntu 20.04, an updated OpenCV (4.5) is needed.
+For building on Ubuntu 20.04, an updated OpenCV (4.5+) is needed.
 
-(need from apt: libcudnn8-dev, cuda? others?)
+### Cuda
 
-git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
-cd opencv
-mkdir build
-cd build
-cmake ../ -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules
-make -j4
-sudo make install
+If using cuda, you may want to install the latest version.
 
-(May need to speciy gcc-8/g++-8 for cuda)
+To see what's installed:
 
-This will install OpenCV in /usr/local
+    apt list --installed | grep cuda
+
+From https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#ubuntu
+
+    sudo apt-key del 7fa2af80
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+    sudo dpkg -i cuda-keyring_1.1-1_all.deb
+    sudo apt update
+
+As of this writing, OpenCV will not build using Cuda 12.2, so install 12.1.
+
+    sudo apt install cuda-12-1
+
+Remove metapackage that installs latest cuda:
+
+    sudo apt remove cuda
+    sudo apt autoremove
+
+Add following to ~/.bashrc and make sure to source it or reboot.
+
+    export PATH=/usr/local/cuda-12.1/bin${PATH:+:${PATH}}
+
+### Clone OpenCV from github
+
+    cd ~/src
+    git clone https://github.com/opencv/opencv.git
+    git clone https://github.com/opencv/opencv_contrib.git
+    cd opencv
+    mkdir build
+    cd build
+
+Without cuda:
+
+    cmake ../ -D CMAKE_INSTALL_PREFIX=~/.local -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules
+
+With cuda:
+
+    cmake ../ -D CMAKE_INSTALL_PREFIX=~/.local -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules -D OPENCV_DNN_CUDA=ON -D WITH_CUDA=ON
+
+
+    make -j4
+    make install
+
+This will install OpenCV in ~/.local
 
 To avoid link errors, the vision_opencv package should also be built against this alternate OpenCV so clone it into your workspace.
 
@@ -25,8 +61,4 @@ https://github.com/rolker/vision_opencv
 
 ## Weights
 
-Weights should be downloaded into the config folder.
-
-From https://github.com/AlexeyAB/darknet/wiki/YOLOv4-model-zoo
-
-Yolo4 weights: https://drive.google.com/open?id=1L-SO373Udc9tPz5yLkgti5IAXFboVhUt
+See onnx model zoo
--- src/opencv_dnn.cpp
+++ src/opencv_dnn.cpp
@@ -45,7 +45,8 @@ public:
         nodeHandle.advertise<std_msgs::String>("class_labels", 1, 0);
 
 
-        nodeHandle.param("threshold/value", m_threshold, (float)0.3);
+        nodeHandle.param("threshold", m_threshold, (float)0.3);
+        ROS_INFO_STREAM("Threshold: " << m_threshold);
         
         std::string model, model_path;
         nodeHandle.param("model", model, std::string());
@@ -95,7 +96,26 @@ public:
         //m_net.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
         // m_net.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA);
         m_output_names = m_net.getUnconnectedOutLayersNames();
-        
+        for(auto out_name: m_output_names)
+            ROS_INFO_STREAM("  output name: " << out_name);
+
+        auto unconnected = m_net.getUnconnectedOutLayers();
+        for(auto l: unconnected)
+        {
+            ROS_INFO_STREAM("  unconnected layer: " << l);
+            std::vector<cv::dnn::MatShape> in_shapes;
+            std::vector<cv::dnn::MatShape> out_shapes;
+            m_net.getLayerShapes(net_input_shape, l, in_shapes, out_shapes);
+            for(auto out_shape: out_shapes)
+            {   
+                if(m_output_shape.empty())
+                    m_output_shape = out_shape;
+                ROS_INFO_STREAM("    " << out_shape.size() << " dimensions");
+                for(auto dim: out_shape)
+                    ROS_INFO_STREAM("      dim: " << dim);
+            }
+        }
+
         m_detectionsPublisher =
         nodeHandle.advertise<vision_msgs::Detection2DArray>("detections", 1, 0);
         
@@ -115,6 +135,7 @@ private:
     float m_threshold;
     cv::dnn::Net m_net;
     std::vector<std::string> m_output_names;
+    std::vector<int> m_output_shape;
     ros::Publisher m_detectionsPublisher;
     image_transport::Publisher m_detectionImagePublisher;
     image_transport::Subscriber m_imageSubscriber;
@@ -154,44 +175,51 @@ private:
         auto dnn_start = std::chrono::steady_clock::now();
         m_net.forward(detections, m_output_names);
         auto dnn_end = std::chrono::steady_clock::now();
-        
-        std::vector<std::vector<int> > indices;
-        indices.resize(m_classLabels.size());
-        std::vector<std::vector<cv::Rect> > boxes;
-        boxes.resize(m_classLabels.size());
-        std::vector<std::vector<float> > scores;
-        scores.resize(m_classLabels.size());
-        
-        for (auto& output : detections)
-        {
-            const auto num_boxes = output.rows;
-            for (int i = 0; i < num_boxes; i++)
-            {
-                auto x = output.at<float>(i, 0) * cv_ptr->image.cols;
-                auto y = output.at<float>(i, 1) * cv_ptr->image.rows;
-                auto width = output.at<float>(i, 2) * cv_ptr->image.cols;
-                auto height = output.at<float>(i, 3) * cv_ptr->image.rows;
-                cv::Rect rect(x - width/2, y - height/2, width, height);
 
-                for (int c = 0; c < m_classLabels.size(); c++)
+        int class_count = m_output_shape[2]-5;
+        std::vector<std::vector<cv::Rect> > boxes(class_count);
+        std::vector<std::vector<float> > scores(class_count);
+        
+        float max_confidence = 0.0;
+
+        for (auto& detection: detections)
+        {
+            for(int i = 0; i < m_output_shape[0]; i++)
+                for(int j = 0; j < m_output_shape[1]; j++)
                 {
-                    auto confidence = *output.ptr<float>(i, 5 + c);
-                    if (confidence >= m_threshold)
+                    max_confidence = std::max(max_confidence, detection.at<float>(i,j,4));
+                    if(detection.at<float>(i,j,4) > m_threshold) // object confidence
                     {
-                        boxes[c].push_back(rect);
-                        scores[c].push_back(confidence);
+                        //ROS_INFO_STREAM("j: " << j << " obj conf: " << detection.at<float>(i,j,4));
+                        auto x = detection.at<float>(i,j, 0) * cv_ptr->image.cols/float(m_width);
+                        auto y = detection.at<float>(i,j, 1) * cv_ptr->image.rows/float(m_height);
+                        auto width = detection.at<float>(i,j, 2) * cv_ptr->image.cols/float(m_width);
+                        auto height = detection.at<float>(i,j, 3) * cv_ptr->image.rows/float(m_height);
+                        cv::Rect rect(x - width/2, y - height/2, width, height);
+                        //ROS_INFO_STREAM("  x,y: " << x << "," << y << " size: " << width << "x" << height);
+                        for(int class_id = 0; class_id < class_count; class_id++)
+                            if(detection.at<float>(i,j,4+class_id) > m_threshold)
+                            {
+                               //ROS_INFO_STREAM("  class: " << class_id << " conf: " << detection.at<float>(i,j,4+class_id));
+                                boxes[class_id].push_back(rect);
+                                scores[class_id].push_back(detection.at<float>(i,j,4+class_id));
+                            }
+           
                     }
                 }
-            }
+
         }
 
-        for (int c = 0; c < m_classLabels.size(); c++)
+        //ROS_INFO_STREAM("max confidence: " << max_confidence);
+
+        std::vector<std::vector<int> > indices(class_count);
+        for (int c = 0; c < class_count; c++)
             cv::dnn::NMSBoxes(boxes[c], scores[c], 0.0, NMS_THRESHOLD, indices[c]);
 
         vision_msgs::Detection2DArray detections_msg;
         detections_msg.header = msg->header;
 
-        for (int c= 0; c < m_classLabels.size(); c++)
+        for (int c= 0; c < class_count; c++)
         {
             for (size_t i = 0; i < indices[c].size(); ++i)
             {
@@ -202,7 +230,11 @@ private:
                 cv::rectangle(cv_ptr->image, cv::Point(rect.x, rect.y), cv::Point(rect.x + rect.width, rect.y + rect.height), color, 3);
 
                 std::ostringstream label_ss;
-                label_ss << m_classLabels[c] << ": " << std::fixed << std::setprecision(2) << scores[c][idx];
+                if(m_classLabels.size() > c)
+                    label_ss << m_classLabels[c];
+                else
+                    label_ss << c;
+                label_ss << ": " << std::fixed << std::setprecision(2) << scores[c][idx];
                 auto label = label_ss.str();
                 
                 int baseline;
